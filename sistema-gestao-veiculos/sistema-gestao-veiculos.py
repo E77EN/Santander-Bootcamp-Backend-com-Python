573172fab9ec21b492bbb983278bdb69db5f077e
E77EN <ellenrfr@gmail.com>
2025-07-31 09:57:25 -0300

    Update sistema-gestao-veiculos.py

--- sistema-gestao-veiculos/sistema-gestao-veiculos.py
+++ sistema-gestao-veiculos/sistema-gestao-veiculos.py
@@ -1,21 +1,24 @@
+import datetime
+
 class Veiculo:
     def __init__(self, marca, modelo, ano):
         self.marca = marca
         self.modelo = modelo
-        self.ano = int(ano)
+        self.ano = ano
 
     def verificar_antiguidade(self):
-        ano_base = 2024  # Use o ano que a plataforma estiver usando para testar
-        if ano_base - self.ano > 20:
+        ano_atual = datetime.datetime.now().year
+        if ano_atual - self.ano > 20:
             return "Veículo antigo"
         else:
             return "Veículo novo"
 
-# Entrada padrão via input
 marca = input()
 modelo = input()
-ano = input()
+ano = int(input())
 
-# Criação do objeto e saída
 veiculo = Veiculo(marca, modelo, ano)
 print(veiculo.verificar_antiguidade())
+
+
+
